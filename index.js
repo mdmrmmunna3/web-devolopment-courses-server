6f11ce96037f52633d7063df654fc8185eafc276
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2022-10-25 22:10:33 +0600

    course -category call dynamically

--- index.js
+++ index.js
@@ -5,17 +5,27 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const course = require('./data/Course.json')
+const courses = require('./data/Courses.json')
+
+const categorys = require('./data/Course-cateory.json')
 
 app.get('/', (req, res) => {
-  res.send('Web development course server is running ');
+    res.send('Web development course server is running ');
 })
 
-// call course.json 
-app.get('/course', (req, res) => {
-    res.send(course)
+// call course-category.json 
+app.get('/category', (req, res) => {
+    res.send(categorys)
+})
+
+// dynamically call
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id
+    const courseCategory = categorys.find(category => category.id === id)
+    console.log(id, courseCategory)
+    res.send(courseCategory)
 })
 
 app.listen(port, () => {
-  console.log(`Web development course server running on port ${port}`)
+    console.log(`Web development course server running on port ${port}`)
 })
